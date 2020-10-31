7103d855c3ebf8b0fd154f397374cfadd7df9af7
JaeYeopHan <ljyhanll@gmail.com>
2020-11-01 01:44:06 +0900

    feat: add script

--- src/__tests__/maskingEmail.test.ts
+++ src/__tests__/maskingEmail.test.ts
@@ -4,5 +4,5 @@ test('maskingEmail', () => {
   const email = "feconf.kr@gmail.com";
   const result = maskingEmail(email);
 
-  expect(result).toBe("fec*****r @ gm*******");
+  expect(result).toBe("fec*****r@gm*******");
 });
