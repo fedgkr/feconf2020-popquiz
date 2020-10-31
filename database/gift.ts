f6fb5ecb5726d68e9c3329fce841b68cd2db3551
soso <ethdud1@gmail.com>
2020-10-31 23:44:07 +0900

    refactor: update 경품type

--- database/gift.ts
+++ database/gift.ts
@@ -1,8 +1,31 @@
-export const 경품 = {
-    1: 'iPhone12',
-    2: 'iPad Air',
-    3: 'Apple Watch Series 6',
-    4: 'Airpods Pro',
-    5: 'Airpods Pro',
-    6: 'Airpods Pro',
-};
+export interface GiftInfo {
+  rank: number;
+  giftName: string;
+}
+
+export const 경품: GiftInfo[] = [
+  {
+    rank: 1,
+    giftName: 'iPhone12'
+  },
+  {
+    rank: 2,
+    giftName: 'iPad Air'
+  },
+  {
+    rank: 3,
+    giftName: 'Apple Watch Series 6'
+  },
+  {
+    rank: 4,
+    giftName: 'Airpods Pro'
+  },
+  {
+    rank: 4,
+    giftName: 'Airpods Pro'
+  },
+  {
+    rank: 4,
+    giftName: 'Airpods Pro'
+  },
+];
