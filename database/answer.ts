bb2406687693f6ab27b58332a1aabc5e8762a637
JaeYeopHan <ljyhanll@gmail.com>
2020-10-31 20:19:25 +0900

    feat: update answer data

--- database/answer.ts
+++ database/answer.ts
@@ -7,19 +7,19 @@ export const 정답 = {
     /**
      * 1번째 A Track:: 이현섭님 - 웹뷰에서 다크모드 상속받기: 일관성 있는 사용자 경험을 위하여
      */
-    answer_a_1: ['user agent', 'User Agent', 'User-Agent', 'USER AGENT', 'User agent'],
+    answer_a_1: ["User Agent", "user agent", "User-Agent", "User agent", "useragent", "user Agent", "USER AGENT", "user-agent", "UserAgent"],
     /**
      * 1번째 B Track:: 최종택님 - React Native에서 Pinch Zoom을 구현하면서 배운것들
      */
-    answer_b_1: ['PanResponderRelease', 'onPanResponderRelease', 'penResponderRelease'],
+    answer_b_1: ['PanResponderRelease'],
     /**
      * 2번째 A Track:: 이한님 - 한글 파일로부터 우리를 구원해줄 hwp.js
      */
-    answer_a_2: ['XMLHttpRequest', 'XmlHttpRequest', 'xmlhttprequest', 'XMLHTTPREQUEST',, 'xmlHttpRequest'],
+    answer_a_2: ['XMLHttpRequest'],
     /**
      * 2번째 B Track:: 최태건님 - OpenAPI Specification으로 타입-세이프하게 API 개발하기 - 희망편 vs 절망편
      */
-    answer_b_2: ['mappings', 'type-mappings'],
+    answer_b_2: ['mappings'],
     /**
      * 3번째 A Track:: 나윤환님 - iframe을 활용하여 전혀 다른 Service를 통합하기
      */
@@ -31,27 +31,27 @@ export const 정답 = {
     /**
      * 4번째 A Track:: 원지혁님 - Relay, 그리고 Declarative에 대해 다시 생각하기
      */
-    answer_a_4: ['Co-location', 'co-location', 'co location', 'colocation', 'Colocation'],
+    answer_a_4: ["co-location", "Co-location", "co location", "co-location ", " Co-location", "colocation", "CoLocation", "Colocation", "Co location", "Co Location"],
     /**
      * 4번째 B Track:: 이성원님 - 프런트엔드를 위한 API 프로토콜, REST만이 답은 아니다. (with. gRPC, GraphQL)
      */
-    answer_b_4: ['Protocol Buffers', 'ProtoBuf'],
+    answer_b_4: ["protocol buffers", "ProtoBuf", "Protocol Buffers", "protoBuf", "Protocol Buffers ", "Protocol buffers", "Protobuf", "protobuf", " Protocol Buffers", "Protocol Buffers(Protobuf)"],
     /**
      * 5번째 A Track:: 최수형님 - 프런드엔드에서 TDD가 가능하다는 것을 보여드립니다
      */
-    answer_a_5: ['Reflection', 'reflection'],
+    answer_a_5: ["Reflection", "reflection"],
     /**
      * 5번째 B Track:: 심흥운님 - Chrome DevTools Protocol을 활용하여 Debugger 개발하기
      */
-    answer_b_5: ['setBreakpoint', 'setbreakpoint'],
+    answer_b_5: ['setBreakpoint'],
     /**
      * 6번째 A Track:: 김혜성님 - Statecharts: 복잡한 UI 상태를 지배하는 방법
      */
-    answer_a_6: ['broadcasting'],
+    answer_a_6: ["Broadcasting", "브로드캐스트커뮤니케이션", "broadcast-communication", "broadcast communication", "브로드캐스트 커뮤니케이션", "broadcasting", "Broadcast communication", "Broadcast Communication", "Broadcast-communication", "Broadcasting (브로드캐스트 커뮤니케이션)", "broadcastcommunication", "브로드캐스트", "broadcast", "Broadcast", "브로드캐스트커뮤니케이션(broadcast-communication)", "broadcast(브로드캐스트)"],
     /**
      * 6번째 B Track:: 김성현님 - 복잡한 백오피스에서 Form의 상태 다루기
      */
-    answer_b_6: ['ImperativeHandle', 'useImperativeHandle'],
+    answer_b_6: ['ImperativeHandle'],
 
     /**
      * 보너스 - 토스 영상 문제
