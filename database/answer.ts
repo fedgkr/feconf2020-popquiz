9073ae56807b778e3bb586e6b34febe3c9422a2f
JaeYeopHan <ljyhanll@gmail.com>
2020-10-31 19:29:17 +0900

    initial commit

---
+++ database/answer.ts
@@ -0,0 +1,85 @@
+/* eslint-disable no-sparse-arrays */
+export const 정답 = {
+    /**
+     * 몸풀기 정답
+     */
+    answer_0: ['Home', 'home', 'HOME'],
+    /**
+     * 1번째 A Track:: 이현섭님 - 웹뷰에서 다크모드 상속받기: 일관성 있는 사용자 경험을 위하여
+     */
+    answer_a_1: ['user agent', 'User Agent', 'User-Agent', 'USER AGENT', 'User agent'],
+    /**
+     * 1번째 B Track:: 최종택님 - React Native에서 Pinch Zoom을 구현하면서 배운것들
+     */
+    answer_b_1: ['PanResponderRelease', 'onPanResponderRelease', 'penResponderRelease'],
+    /**
+     * 2번째 A Track:: 이한님 - 한글 파일로부터 우리를 구원해줄 hwp.js
+     */
+    answer_a_2: ['XMLHttpRequest', 'XmlHttpRequest', 'xmlhttprequest', 'XMLHTTPREQUEST',, 'xmlHttpRequest'],
+    /**
+     * 2번째 B Track:: 최태건님 - OpenAPI Specification으로 타입-세이프하게 API 개발하기 - 희망편 vs 절망편
+     */
+    answer_b_2: ['mappings', 'type-mappings'],
+    /**
+     * 3번째 A Track:: 나윤환님 - iframe을 활용하여 전혀 다른 Service를 통합하기
+     */
+    answer_a_3: ['sandbox'],
+    /**
+     * 3번째 B Track:: 이경주님 - 확장 가능한 CSS를 재사용하여 스타일링하기
+     */
+    answer_b_3: ['key'],
+    /**
+     * 4번째 A Track:: 원지혁님 - Relay, 그리고 Declarative에 대해 다시 생각하기
+     */
+    answer_a_4: ['Co-location', 'co-location', 'co location', 'colocation', 'Colocation'],
+    /**
+     * 4번째 B Track:: 이성원님 - 프런트엔드를 위한 API 프로토콜, REST만이 답은 아니다. (with. gRPC, GraphQL)
+     */
+    answer_b_4: ['Protocol Buffers', 'ProtoBuf'],
+    /**
+     * 5번째 A Track:: 최수형님 - 프런드엔드에서 TDD가 가능하다는 것을 보여드립니다
+     */
+    answer_a_5: ['Reflection', 'reflection'],
+    /**
+     * 5번째 B Track:: 심흥운님 - Chrome DevTools Protocol을 활용하여 Debugger 개발하기
+     */
+    answer_b_5: ['setBreakpoint', 'setbreakpoint'],
+    /**
+     * 6번째 A Track:: 김혜성님 - Statecharts: 복잡한 UI 상태를 지배하는 방법
+     */
+    answer_a_6: ['broadcasting'],
+    /**
+     * 6번째 B Track:: 김성현님 - 복잡한 백오피스에서 Form의 상태 다루기
+     */
+    answer_b_6: ['ImperativeHandle', 'useImperativeHandle'],
+
+    /**
+     * 보너스 - 토스 영상 문제
+     */
+    bonus_1: ['the WORK'],
+    /**
+     * 보너스 - 네이버 영상 문제
+     */
+    bonus_2: ['슬랙 (Slack)'],
+    /**
+     * 보너스 - 쿠팡 영상 문제
+     */
+    bonus_3: ['coupang tech'],
+    /**
+     * 보너스 - 우아한형제들 영상 문제
+     */
+    bonus_4: ['부릉'],
+    /**
+     * 보너스 - 당근마켓 영상 문제
+     */
+    bonus_5: ['로컬 슈퍼앱'],
+    /**
+     * 보너스 - EJN 영상 문제
+     */
+    bonus_6: ['크리에이터를 위한 플랫폼과 도구'],
+    /**
+     * 보너스 - 클래스101 영상 문제
+     */
+    bonus_7: ['모두가 사랑하는 일을 하며 살 수 있도록'],
+};
+
---
+++ src/index.ts
@@ -0,0 +1,89 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const csv = require('csvtojson');
+const { 정답 } = require("../database/answer");
+
+const csvFilePath = './database/data.csv';
+
+interface DataRow {
+  id?: string,
+  timestamp?: string;
+  question?: string;
+  answer_0?: string;
+  answer_a_1?: string;
+  answer_b_1?: string;
+  answer_a_2?: string;
+  answer_b_2?: string;
+  answer_a_3?: string;
+  answer_b_3?: string;
+  answer_a_4?: string;
+  answer_b_4?: string;
+  answer_a_5?: string,
+  answer_b_5?: string;
+  answer_a_6?: string;
+  answer_b_6?: string;
+  bonus_1?: string;
+  bonus_2?: string;
+  bonus_3?: string;
+  bonus_4?: string;
+  bonus_5?: string;
+  bonus_6?: string;
+  bonus_7?: string;
+}
+
+(async function main() {
+  const jsonDataset = await getData();
+  const dataset = getMapDataset(jsonDataset);
+  const scoreById = getScoreById(dataset);
+
+  console.groupCollapsed('[참여한 사람] ------> ', scoreById.size);
+  console.log(scoreById);
+  console.groupEnd();
+})();
+
+function getData(): Promise<DataRow[]> {
+  return new Promise((resolve) => {
+    csv()
+      .fromFile(csvFilePath)
+      .then(resolve);
+  });
+}
+
+function getMapDataset(jsonDataset) {
+  const dataset = new Map<string, Omit<DataRow, 'id'>>();
+
+  jsonDataset.forEach(jsonData => {
+    const { id, question, ...data } = jsonData;
+    const refinedData = Object.entries(data).reduce((result: DataRow , [key, value]) => {
+      // * 입력되지 않은 값 제거, 불필요한 field 제거
+      if (value == null || value == '' || key === 'timestamp') {
+        return result;
+      }
+      
+      return { ...result, [key]: value };
+    }, {});
+
+    const value = dataset.has(id) ? { ...dataset.get(id), ...refinedData } : refinedData;
+    
+    dataset.set(id, value);
+  });
+
+  return dataset;
+}
+
+function getScoreById(dataset: Map<string, Omit<DataRow, 'id'>>) {
+  const scoreById = new Map<string, number>();
+  
+  Array.from(dataset.entries()).map(([id, answerObject]) => {
+    const score = Object.entries(answerObject).reduce((score, [questionId, answer]) => {
+      const answers = 정답[questionId];
+
+      return answers?.includes(answer) ? score + 1 : score;
+    }, 0);
+
+    scoreById.set(id, score);
+  });
+  const sorted = Array.from(scoreById.entries()).sort(([aId, aScore], [bId, bScore]) => bScore - aScore);
+
+  return new Map(sorted);
+}
