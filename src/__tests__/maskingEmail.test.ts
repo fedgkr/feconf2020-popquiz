ad1e03416e57783be743a62e60e37850aeeb4a08
JaeYeopHan <ljyhanll@gmail.com>
2020-11-01 00:16:26 +0900

    test: add maskingEmail test

---
+++ src/__tests__/maskingEmail.test.ts
@@ -0,0 +1,8 @@
+import { maskingEmail } from '../index';
+
+test('maskingEmail', () => {
+  const email = "feconf.kr@gmail.com";
+  const result = maskingEmail(email);
+
+  expect(result).toBe("fec*****r @ gm*******");
+});
